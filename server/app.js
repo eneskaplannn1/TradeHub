f0e05e8d3f450d26a7619a9dabbf2e31e7a99bd3
eneskaplannn1 <muhammedenes.kaplan@std.yeditepe.edu.tr>
2023-10-21 22:35:34 +0300

    fixed bug

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(
   cors({
-    origin: ['https://tradehub-3t3s.onrender.com/'],
+    origin: ['http://localhost:5173'],
     methods: ['POST', 'PATCH', 'GET', 'DELETE', 'PUT'],
     credentials: true,
   })
